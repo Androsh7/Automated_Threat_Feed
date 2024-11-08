dcc368aaae326a6e34911ba09e6156f2dc1477c0
Androsh7 <Androsh7@gmail.com>
2024-11-07 17:34:13 -0800

    Update

--- web_scraper.py
+++ web_scraper.py
@@ -4,13 +4,8 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 from datetime import timedelta
 import pytz
-
-from llama_cpp import Llama
-
-llm = Llama.from_pretrained(
-	repo_id="dabr1/Llama-3.1-8B-Lexi-Uncensored_V2-Q4_0_4_8.gguf",
-	filename="Llama-3.1-8B-Lexi-Uncensored_V2-Q4_0_4_8.gguf",
-)
+import json
+import re
 
 '''
 https://github.com/Androsh7/python_web_scraper
@@ -38,19 +33,66 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
+def remove_html_tags(text):
+    pattern = re.compile(r'<.*?>')
+    return re.sub(pattern, '', text)
+
 # this is a list of all urls to be searched along with the required XML locations
 # format: url, news_block_location, news_title_location, news_link_location, news_pub_date_location, news_pub_date_format
 urllist = [
-    ['https://thehackernews.com/news-sitemap.xml', '{http://www.sitemaps.org/schemas/sitemap/0.9}url', '{http://www.google.com/schemas/sitemap-news/0.9}news/{http://www.google.com/schemas/sitemap-news/0.9}title' , '{http://www.sitemaps.org/schemas/sitemap/0.9}loc', '{http://www.google.com/schemas/sitemap-news/0.9}news/{http://www.google.com/schemas/sitemap-news/0.9}publication_date', '%Y-%m-%dT%H:%M:%S%z']
-    #,['https://news.google.com/rss/search?hl=en-US&gl=US&ceid=US:en&q=cyber', 'channel/item', 'title' ,'link', 'pubDate', '%a, %d %b %Y %H:%M:%S %Z']
+    'https://thehackernews.com/news-sitemap.xml',
+    'https://cyberscoop.com/post-sitemap7.xml',
 ]
 
-news_cutoff = datetime.now() - timedelta(hours= 24)
+
+news_cutoff = datetime.now() - timedelta(hours= 8)
 tz = pytz.timezone('America/Los_Angeles')
 
 # this iterates through the urllist and grabs all links for sites within the cuttoff time
 link_list = []
+
 for news_site in urllist:
+    r = requests.get(news_site)
+    xml = r.text
+    soup = BeautifulSoup(xml, "lxml")
+    
+    try:
+        xml_soup = soup.findAll('url')
+    except:
+        print("ERROR GRABBING SITE BLOCKS, ABORTING OPERATION")
+        continue
+    
+    for link in xml_soup:
+        # grab url
+        try:
+            linkstr = link.find('loc').getText('', True)
+        except:
+            print("ERROR ATTEMPTING TO GRAB URL FROM XML DATA")
+            continue
+        
+        # grab last modified date
+        try:
+            linkdate = link.find('lastmod').getText('', True)
+        except:
+            print("ERROR ATTEMPTING TO GRAB LAST MODIFIED DATE FROM XML DATA")
+            continue
+            
+        # grab the title
+        try:
+            linktitle = link.find('title').getText('', True)
+        except:
+            # Grab title from url
+            linktitle = linkstr.rstrip("/ ").split("/")[-1]
+            
+        pub_date_std = datetime.strptime(linkdate, '%Y-%m-%dT%H:%M:%S%z') # converts date format to datetime object
+        # quits if news is past cutoff date
+        if (pub_date_std.astimezone(tz) < news_cutoff.astimezone(tz)):
+            continue
+        
+        link_list.append({"link" : linkstr, "date" : linkdate, "title" : linktitle})
+for line in link_list:
+    print(line)
+'''for news_site in urllist:
     response = requests.get(news_site[0])
     xml = response.text
     root = ET.fromstring(xml)
@@ -61,14 +103,38 @@ for news_site in urllist:
         # quits if news is past cutoff date
         if (pub_date_std.astimezone(tz) < news_cutoff.astimezone(tz)):
             continue
-        title = child.find(news_site[2]).text # gets the title for the article
+        
         url = child.find(news_site[3]).text # gets the url for the article
-        link_list.append([url, title, pub_date_std]) # adds the url, title, pubdate to the link_list
-
+        
+        if (news_site[2] == "NONE"):
+            title = url.split("/")[-2]
+        else:
+            title = child.find(news_site[2]).text # gets the title for the article    
+        
+        link_list.append([url, title, pub_date]) # adds the url, title, pubdate to the link_list
+print(link_list)'''
+exit()
 # this grabs the body of the article and adds it to the link list
 for link in link_list:
     html = requests.get(link[0], allow_redirects=True)
     soup = BeautifulSoup(html.content, "html.parser")
-    link.append(soup.find_all("p"))
-    print(link[0], link[1], link[2], link[3])
-    print("-" * 20)
+    raw_data = soup.find_all("p")
+    
+    # remove html tags
+    stripped_html = remove_html_tags(str(raw_data))
+
+    payload = {
+        "prompt" : stripped_html + ". Summary of the article that is a MAXIMUM of 3 sentences:",
+        "temperature" : 0.2,
+        "top_p" : 0.9,
+        "max_content_length" : 2048,
+        "max_length" : 256,
+        "n" : 1,
+    }
+    #print(payload)
+    r = requests.post('http://localhost:5001/api/v1/generate', data=json.dumps(payload))
+    print('-' * 10, link[1], "-" , link[2])
+    json_data = r.json()
+    link.append(json_data["results"][0]["text"].lstrip(", ["))
+
+# this will write the automated threat feed to a file
